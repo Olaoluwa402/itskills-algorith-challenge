1beffac7c0a255d08cbb7eb9955fb4b5d703f07c
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2022-05-20 10:41:03 +0100

    chucks solution

--- main.js
+++ main.js
@@ -147,3 +147,34 @@ function convertToFahrenheit(C) {
         }
 
         // console.log(findElement([1, 3, 5, 5, 10, 11], function(num) { return num % 2 === 0; }))
+
+        /*
+                Write a function that splits an array (first argument) into groups the length of size (second argument) 
+                and returns them as a two-dimensional array.
+
+                Example:
+                chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
+
+                chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
+
+                chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].
+
+                chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
+
+        */
+
+    function chucnks(arr, size){
+        // let newArray = [];
+        // while(arr.length > 0){
+        //      newArray.push(arr.splice(0, size))
+        // }
+        // return newArray;
+        //alternative
+        let newArray = []
+        for(let i=0; i<arr.length; i+=size){
+            newArray.push(arr.slice(i, i+size))
+        }
+        return newArray
+    }
+
+    console.log(chucnks([0, 1, 2, 3, 4, 5], 4))
